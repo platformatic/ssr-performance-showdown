23898b97594edbf6502891553da974733c105656
Jonas Galvez <jonasgalvez@gmail.com>
2024-08-28 16:30:06 -0300

    rm idCounter from vue version

--- vue/client/base.vue
+++ vue/client/base.vue
@@ -2,9 +2,8 @@
   <div id="wrapper">
     <div
       v-for="(tile, index) in tiles"
-      :key="tile.id"
       class="tile"
-      :style="{ left: tile.x + 'px', top: tile.y + 'px' }"
+      :style="`left: ${tile.x.toFixed(2)}px; top: ${tile.y.toFixed(2)}px`"
     ></div>
   </div>
 </template>
@@ -16,9 +15,6 @@ const cellSize = 10
 const centerX = wrapperWidth / 2
 const centerY = wrapperHeight / 2
 
-const tiles = []
-
-let idCounter = 0
 let angle = 0
 let radius = 0
 
@@ -32,7 +28,7 @@ while (radius < Math.min(wrapperWidth, wrapperHeight) / 2) {
   y = centerY + Math.sin(angle) * radius
 
   if (x >= 0 && x <= wrapperWidth - cellSize && y >= 0 && y <= wrapperHeight - cellSize) {
-    tiles.push({ x, y, id: idCounter++ })
+    tiles.push({ x, y })
   }
 
   angle += 0.2
